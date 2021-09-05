0b0453cde08d6863e8e1f00e77856dbb6928d988
Kwon770 <sckwon770@gmail.com>
2021-09-05 15:14:57 +0900

    refactor: Increase expiration period of jwt token

--- src/main/java/com/kwon770/mm/provider/security/JwtTokenProvider.java
+++ src/main/java/com/kwon770/mm/provider/security/JwtTokenProvider.java
@@ -27,8 +27,8 @@ public class JwtTokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 2;    // 2일
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 30;  // 30일
 
     private final Key key;
 
