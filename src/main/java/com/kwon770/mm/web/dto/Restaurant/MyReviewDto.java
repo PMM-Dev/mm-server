e8c51b96004eb3e62da4e69e8025f922c498a8b6
Kwon770 <sckwon770@gmail.com>
2021-10-06 01:40:24 +0900

    refactor: Refactoring MyReviewDto by adding id, createdDate, authorName fields

--- src/main/java/com/kwon770/mm/web/dto/Restaurant/MyReviewDto.java
+++ src/main/java/com/kwon770/mm/web/dto/Restaurant/MyReviewDto.java
@@ -6,21 +6,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.text.SimpleDateFormat;
+
 @AllArgsConstructor
 @Builder
 @Getter
 @NoArgsConstructor
 public class MyReviewDto {
 
+    private Long id;
+    private String createdDate;
+    private String authorName;
     private String description;
     private Float grade;
     private Long restaurantId;
     private String restaurantName;
 
     public MyReviewDto(Review review) {
+        this.id = review.getId();
+        this.authorName = review.getAuthor().getName();
         this.description = review.getDescription();
         this.grade = review.getGrade();
         this.restaurantId = review.getRestaurant().getId();
         this.restaurantName = review.getRestaurant().getName();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
+        createdDate = format.format(review.getCreatedDate());
     }
 }
